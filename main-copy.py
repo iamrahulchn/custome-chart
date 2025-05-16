1316e7b0fd56bfcf89ddbcd9860bec4a17a3ba45
Rahul chauhan <rahulshow2023@gmail.com>
2025-05-16 18:48:56 +0530

    Update main-copy.py

--- main-copy.py
+++ main-copy.py
@@ -5,14 +5,8 @@ from datetime import datetime, time
 from PIL import Image, ImageDraw, ImageFont
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.chrome.service import Service
-from selenium.webdriver.common.by import By
-from selenium.webdriver.support.ui import WebDriverWait
-from selenium.webdriver.support import expected_conditions as EC
-from aiohttp import web
 import os
 import time as t
-import pandas as pd
 
 # --- Configuration ---
 api_id = '28654273'
